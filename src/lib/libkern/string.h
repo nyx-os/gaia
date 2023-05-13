c784cb47eac94d4835606523d6ad789e56786e5d
lg <abbix@riseup.net>
2023-05-13 18:01:31 -0400

    feat(libkern): add string.h

---
+++ src/lib/libkern/string.h
@@ -0,0 +1,14 @@
+#ifndef SRC_LIB_LIBKERN_STRING_H_
+#define SRC_LIB_LIBKERN_STRING_H_
+#include <stdint.h>
+#include <stddef.h>
+
+char *strncpy(char *destination, const char *source, size_t num);
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *dest, int c, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+size_t strlen(const char *s);
+char *strrchr(const char *s, int c);
+int strncmp(const char *s1, const char *s2, size_t n);
+
+#endif
