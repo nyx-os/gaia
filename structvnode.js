9973d07bb4eabd639f5b68d47e3782368e99088c
Abb1x <loicgre@gmail.com>
2023-05-13 01:11:52 +0000

    Deploying to gh-pages from @ nyx-org/gaia@132394202ddedb154158588de007cb7ee8e45ec9 🚀

--- structvnode.js
+++ structvnode.js
@@ -1,5 +1,6 @@
 var structvnode =
 [
+    [ "abs_path", "structvnode.html#ad743d84834dbb11ef75d601802e25bcf", null ],
     [ "data", "structvnode.html#a41bc0f8f618fcbd98b91685d7c8fd193", null ],
     [ "ops", "structvnode.html#acdfdb7c03dde7cc8ee20720084c3f791", null ],
     [ "type", "structvnode.html#af027dc1498376ea9897e2feab9885351", null ]
